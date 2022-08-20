866312c3ba924e6940d534f76a0b758812aad32a
Ali-Khavanin <khavanin.a2015@gmail.com>
2022-08-20 12:24:49 +0430

    reading data is fully developed for legal entities

--- test_class1.py
+++ test_class1.py
@@ -6,15 +6,24 @@ sys.path.append(directory_path+'/digital_factor/digital_factor_base')
 sys.path.append(directory_path+'/digital_factor')
 from digital_factor.l_entity_digital_factor import lEntityDigitalFactor
 
-excel_df = pd.read_excel('digital_factor/digital_factor_base/test_data.xlsx')
-
+df = pd.read_excel('resources/template_data.xlsx')
 data = []
+for index,row in df.iterrows():
+    lEntityDigitalFactor_obj = lEntityDigitalFactor(
+        buy_or_sale=row['نوع'],
+        company_name=row['نام شرکت'],
+        company_code=row['شماره اقتصادی'],
+        province=row['استان'],
+        city=row['شهر'],
+        address = row['آدرس'],
+        kala_name=row['نام کالا'],
+        price=row['مبلغ'],
+        maliat_arzesh_afzoodeh=row['مالیات ارزش افزوده'],
+        avarez_arzesh_afzoodeh=row['عوارض ارزش افزوده']
+
+    )
+    print(lEntityDigitalFactor_obj.get_info())
+    data.append(lEntityDigitalFactor_obj)
 #TODO: configure the excel file to be used more conviniently
 #TODO: double check the architecture of the software and ease of its use
-for index,row in excel_df.iterrows():
-    l_entity = lEntityDigitalFactor()
-    l_entity.fill_info(
-        {
-            
-        }
-    )
+print(data[1].get_info())
