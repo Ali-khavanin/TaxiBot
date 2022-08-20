3c6afda1f89ca231574965424f08e4327b0a89a9
Ali-Khavanin <khavanin.a2015@gmail.com>
2022-08-20 12:19:33 +0430

    improved constructor

--- digital_factor/l_entity_digital_factor.py
+++ digital_factor/l_entity_digital_factor.py
@@ -1,29 +1,29 @@
+from math import nan
 from digital_factor_base import DigitalFactor
 class lEntityDigitalFactor(DigitalFactor): 
-    required_class_info = {
-        "type_of_factor_owner": "lEntityDigitalFactor",
-        "buy_or_sale" : None, #1 is for buy, 0 is for sale
-        "type_of_buyer" : 1, # 1 = مشمول ثبتنام در نظام مالیاتی
-        "company_name" : None,
-        "company_code" : None, #شماره اقتصادی
-        "province" : None,
-        "city" : None,
-        "address" : None,
-        "type_of_kala" : 1 , # سایر کالا ها
-        "kala_name" : None,
-        "type_of_trade" : 1, #ریالی
-        "price" : None,#مبلغ کالا
-        "maliat_arzesh_afzoodeh":None,
-        "avarez_arzesh_afzoodeh":None,
-    }
+    type_of_factor_owner =  "lEntityDigitalFactor"
+    buy_or_sale = None #1 is for buy, 0 is for sale
+    type_of_movadi =  1 # 1 = مشمول ثبتنام در نظام مالیاتی
+    company_name =  None
+    company_code = None #شماره اقتصادی
+    province = None
+    city = None
+    address = None
+    type_of_kala =   1  # سایر کالا ها
+    kala_name = None
+    type_of_trade = 1 #ریالی
+    price = None#مبلغ کالا
+    maliat_arzesh_afzoodeh = None
+    avarez_arzesh_afzoodeh = None
+
 
     def __init__(self,**kwargs):
             for key,value in kwargs.items():
-                if value is not None :
+                if value is not nan :
                     if value is float :
-                        self.required_class_info[key] = int(value)
+                        self.__dict__[key] = int(value)
                     else :
-                        self.required_class_info[key] = value
+                        self.__dict__[key] = value
 
     def fill_info(self, info: dict) :
         for data in info :
@@ -33,5 +33,5 @@ class lEntityDigitalFactor(DigitalFactor):
                 print("Error: data not found in required_class_info")
                 return False
     def get_info(self) :
-        return self.required_class_info
+        return self.__dict__
     
